6259f13bdcc85332cabb1db6e9d43cf596404fda
robotganteng09 <arsyafaisyar@gmail.com>
2025-03-19 21:08:49 +0700

    belajar props

--- src/hello-world/helloworld.jsx
+++ src/hello-world/helloworld.jsx
@@ -1,29 +1,36 @@
 export default function helloWorld(){
+    const props = {
+        text1:"mas tampan"
+    }
     return(
         <div>
-        <HeaderHelloWorld/>
+        <HeaderHelloWorld {...props}/>
         <ParagraphHelloWorld/>
         </div>
     )
 }
 
 //note: bisa memanggil function dan di tampilkan
-function HeaderHelloWorld(){
-    const text1 = "hello"
+function HeaderHelloWorld({text1 = "ayam"}){
+    // const text1 = "hello"
     return(
     <div>
          {/* <h1>Hello</h1> */}
-         <h1>{text1.toUpperCase()}</h1>
+         <h1 style={{
+            color:"red", // bisa memanggil langsung
+            backgroundColor:"aqua"
+         }}>{text1.toUpperCase()}</h1>
     </div>
     )
 }
 
 function ParagraphHelloWorld(){
     const text2 = "selamat datang"
+    const style = {color:"blue",backgroundColor:"red"} // bisa dibungkus terlebih dahulu
     return(
         <div>
       {/* <h2>selamat datang</h2> */}
-      <h2>{text2.toUpperCase()}</h2>
+      <h2 style={style}>{text2.toUpperCase()}</h2>
     </div>
     ) 
 }
