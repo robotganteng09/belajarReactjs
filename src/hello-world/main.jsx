f2e764c596dcbf550da8ac4674b4a092a0868b54
robotganteng09 <arsyafaisyar@gmail.com>
2025-03-21 21:24:27 +0700

    belajar pure component

--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import HelloWorld from "./helloworld.jsx"; // Ubah menjadi huruf besar
 import Container from "./Container.jsx";
 import Todolist from "../todolist/Todolist.jsx";
+import Table from "../table/Table.jsx";
 
 
 createRoot(document.getElementById("root")).render(
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")).render(
     <Container>
         <HelloWorld/>
         <Todolist/>
+        <Table/>
     </Container>
   
 );
