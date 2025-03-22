7272934c6c68d8a1994a30312128620f3293faa0
robotganteng09 <arsyafaisyar@gmail.com>
2025-03-22 10:52:23 +0700

    button click

---
+++ src/button/AlertButton.jsx
@@ -0,0 +1,10 @@
+export default function AlertButton({text,message}){
+    function handleClick(){
+        alert(message)
+    }
+     
+    return(
+        <button onClick={handleClick}>{text}</button>
+    )
+   
+}
--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -3,6 +3,7 @@ import HelloWorld from "./helloworld.jsx"; // Ubah menjadi huruf besar
 import Container from "./Container.jsx";
 import Todolist from "../todolist/Todolist.jsx";
 import Table from "../table/Table.jsx";
+import AlertButton from "../button/alertButton.jsx";
 
 
 createRoot(document.getElementById("root")).render(
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")).render(
     <Container>
         <HelloWorld/>
         <Todolist/>
+       <AlertButton text= "click me" message="lu ganteng"/>
         <Table/>
     </Container>
   
