0280b6af50a5076de04a1bb5f4a0a199205e306f
robotganteng09 <arsyafaisyar@gmail.com>
2025-03-27 16:53:22 +0700

    belajar object state

--- src/conctact/ContactForm.jsx
+++ src/conctact/ContactForm.jsx
@@ -1,6 +1,40 @@
+
+import {useImmer} from "use-immer"
+
+const initialData = {
+    nama: "",
+    Absen: ""
+}
 export default function ContactForm(){
+
+    const[contact,setContact]  = useImmer(initialData)
+
+    function handleNameChange(e){
+       setContact(draft => {
+        draft.nama = e.target.value
+       })
+    }
+
+    function handleAbsenChange(e){
+      setContact(draft => {
+        draft.Absen = e.target.value
+       })
+    }
+
     return(
         <>
+        <div>
+          <h1>contact form</h1>
+          <form>
+            <input type="text" placeholder="Name" value={contact.nama} onChange={handleNameChange}/>
+            <br />
+            <input type="text" placeholder="Absen" value={contact.Absen} onChange={handleAbsenChange}/>
+          </form>
+          <h1>contact detail</h1>
+
+          <p>nama: {contact.nama}</p>
+          <p>absen: {contact.Absen}</p>
+        </div>
         </>
     )
 }
